d5cd3da606015207838cd96a79702dd87d42ae0b
hod <hod246>
2018-04-11 18:46:54 +0300

    added calculator functions to header file

--- header.h
+++ header.h
@@ -23,10 +23,7 @@ void readInput(initData* init, polynom* pol);
 
 complexNumber* f(polynom* pol, complexNumber* z);
 
-polynom* convertDeriv(polynom* pol);
-
-// (Dividend/Divisor) = quotient.remainder
-complexNumber* div(complexNumber* dividend, complexNumber* divisor);
+polynom* convertDeriv(polynom* pol);//hod
 
 complexNumber* getNextZ(complexNumber* z_n, polynom* pol_f, polynom* pol_f_deriv);
 
@@ -35,5 +32,15 @@ bool checkAcc(initData* init, polynom* pol, complexNumber z_n);
 complexNumber* abs(complexNumber* z);
 
 void printResult(complexNumber* root);
+
+// (Dividend/Divisor) = quotient.remainder
+complexNumber* div(complexNumber* dividend, complexNumber* divisor);
+
+complexNumber* mult(complexNumber* first, complexNumber* second);
+
+complexNumber* add(complexNumber* first, complexNumber* second);
+
+complexNumber* sub(complexNumber* first, complexNumber* second);
+
 
 #endif //ASSIGNMENT2_ARCHIT_HEADER_H
--- main.c
+++ main.c
@@ -1,4 +1,16 @@
 //
 // Created by hod on 11/04/18.
 //
+#include "header.h"
+polynom* convertDeriv(polynom* pol){
+    polynom* newPolynom = calloc(1, sizeof(polynom));
+    newPolynom->coeffs = calloc(pol->order, sizeof(complexNumber));
+    newPolynom->order = pol->order-1;
+    for (int i=0;i<pol->order;i++){
+        newPolynom->coeffs[i].real = pol->coeffs[i].real*i;
+        newPolynom->coeffs[i].imagine = pol->coeffs[i].imagine*i;
+    }
+    return newPolynom;
+}
+
 
